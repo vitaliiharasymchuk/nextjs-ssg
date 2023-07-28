1474bc065aaf8cfd15c0e2ba929df579b09f98d8
VysochanskiyS <visosensey@gmail.com>
2023-07-28 10:45:34 +0300

    add: main test features to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
-export default function Home() {
-  return (
-    <main>
-            Test 28.07.2023 9:57
-    </main>
-  )
+async function getData() {
+    const res = await fetch(process.env.URL || "");
+    return res.json();
+}
+
+export default async function Home() {
+    const data = await getData();
+
+    return (
+        <main>
+            WITH ENV AND NETLIFY AT TIME={process.env.WORKFLOW_RUNNING_TIME}
+            {data.map((el: any) => (<div key={el.id}>{el.title}</div>))}
+        </main>
+    )
 }
